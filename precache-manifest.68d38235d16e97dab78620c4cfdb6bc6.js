c8d1e79063346db85068c6bd4d0971db3903a590
JoshuaCode1 <zuma4good@gmail.com>
2020-06-24 23:38:47 +0100

    Updates

---
+++ precache-manifest.68d38235d16e97dab78620c4cfdb6bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f792fdfaa4a7d346c339706a181607",
+    "url": "/React-TodoApp1/index.html"
+  },
+  {
+    "revision": "b5fa98dda20c5a9cd164",
+    "url": "/React-TodoApp1/static/css/main.27e52751.chunk.css"
+  },
+  {
+    "revision": "5e0a7a6d6ff2689c489c",
+    "url": "/React-TodoApp1/static/js/2.40b6b50f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TodoApp1/static/js/2.40b6b50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fa98dda20c5a9cd164",
+    "url": "/React-TodoApp1/static/js/main.937a4bd7.chunk.js"
+  },
+  {
+    "revision": "e0d6982914a50529ee34",
+    "url": "/React-TodoApp1/static/js/runtime-main.f120bf94.js"
+  }
+]);
